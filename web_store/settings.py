f0ca448eda2a4475ead8a94871cb56f90667345a
Tony <tadmopar@gmail,com>
2021-06-07 20:53:56 +0300

    right footer

--- web_store/settings.py
+++ web_store/settings.py
@@ -127,6 +127,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'email'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'tadmopar@gmail.com'
+EMAIL_HOST_PASSWORD = '139420671'
 
