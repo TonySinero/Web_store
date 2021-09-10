55e5b9aa02daac897ae803305d133dde6247e69b
Tony <tadmopar@gmail,com>
2021-09-10 20:45:33 +0300

    last update

--- web_store/settings.py
+++ web_store/settings.py
@@ -8,8 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z1+-htykk8wl$o&0y(bj3g9pg+k6=7asa_jehpt5i_e5w86v#b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
